feff75f3aa507ac8df1319cd89c57b583198dee3
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-11-06 13:35:58 -0500

    analysis on peakiness

--- MiscModelAnalyses/peakLocationDistr.m
+++ MiscModelAnalyses/peakLocationDistr.m
@@ -1,49 +1,72 @@
 addpath('../Func');
 setDir;
-load ([TempDatDir 'DataListS2CModelFix.mat']);
+load ([TempDatDir 'DataListShuffle.mat']);
 
-
-for nData     = 4:length(DataSetList)
-    load([TempDatDir DataSetList(nData).name '.mat'])
-    numTimeBin          = size(nDataSet(1).unit_yes_trial, 2);
-    yesProfileMatrix    = nan(length(nDataSet), numTimeBin);
-    noProfileMatrix     = yesProfileMatrix;
-    for nUnit        = 1:length(nDataSet)
-        yesData      = mean(nDataSet(nUnit).unit_yes_trial);
-        noData       = mean(nDataSet(nUnit).unit_no_trial);
-        maxData      = max([yesData, noData]);
-        minData      = min([yesData, noData]);
-        rData        = (maxData - minData);
-        yesData      = (yesData - minData)/(maxData - minData);
-        noData       = (noData - minData)/(maxData - minData);
-        yesProfileMatrix(nUnit, :)    = yesData;
-        noProfileMatrix(nUnit, :)     = noData;
-    end
-    actMat        = [yesProfileMatrix, noProfileMatrix];
-    [~, maxId]    = max(actMat, [], 2);
-
-    timeStep  = DataSetList(nData).params.timeSeries;
-    numTime   = length(timeStep);
-    polein    = DataSetList(nData).params.polein;
-    poleout   = DataSetList(nData).params.poleout;
-    timeTag   = 8:numTimeBin;
-    
-    countMaxId = hist(maxId, 1:numTimeBin*2)/length(nDataSet)*100;
-    disp(sqrt(mean((countMaxId - 1/numTimeBin/2*100).^2)))
-    [bootstat,bootsam] = bootstrp(1000,@std,countMaxId([timeTag, timeTag+numTimeBin]));
-    std(bootstat)
-%     figure;
-%     hold on;
-%     plot(timeStep, countMaxId(1:numTimeBin), '-', 'linewid', 1.0, 'color', [0.7 0 0]);
-%     plot(timeStep, countMaxId(1+numTimeBin:end), '-', 'linewid', 1.0, 'color', [0 0 0.7]);
-%     xlim([timeStep(1) 2])
-%     ylim([0 6])
-%     gridxy ([polein, poleout, 0],[1/numTimeBin/2*100], 'Color','k','Linestyle','--','linewid', 1.0)
-%     box off
-%     ylabel('% Max peak')
-%     xlabel('Time')
-%     hold off
-%     setPrint(8, 3, [PlotDir 'S2CModel\SingleUnitsMaxLocation_' DataSetList(nData).name])
+if ~exist([PlotDir 'SingleUnitsPeakLocation'],'dir')
+    mkdir([PlotDir 'SingleUnitsPeakLocation'])
 end
 
-close all
+nData     = 1;
+if ~exist([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'], 'file')
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    neuronRemoveList = false(length(nDataSet), 1);
+else
+    load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
+end
+
+disp(DataSetList(nData).name)
+params      = DataSetList(nData).params;
+timePoints  = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+
+numTimeBin          = size(nDataSet(1).unit_yes_trial, 2);
+yesProfileMatrix    = nan(length(nDataSet), numTimeBin);
+noProfileMatrix     = yesProfileMatrix;
+positivePeak        = false(length(nDataSet),1);
+
+for nUnit        = 1:length(nDataSet)
+    yesData      = mean(nDataSet(nUnit).unit_yes_trial);
+    noData       = mean(nDataSet(nUnit).unit_no_trial);
+    maxData      = max([yesData, noData]);
+    minData      = min([yesData, noData]);
+    rData        = (maxData - minData);
+    yesData      = (yesData - minData)/(maxData - minData);
+    noData       = (noData - minData)/(maxData - minData);
+    yesProfileMatrix(nUnit, :)    = yesData;
+    noProfileMatrix(nUnit, :)     = noData;
+    positivePeak(nUnit)       = mean(yesData(timePoints(1):timePoints(2))) <= mean(yesData(timePoints(2):timePoints(4))) ...
+                               || mean(noData(timePoints(1):timePoints(2))) <= mean(noData(timePoints(2):timePoints(4)));
+
+end
+actMat        = [yesProfileMatrix, noProfileMatrix];
+actMat        = actMat(positivePeak, :);
+[~, maxId]    = max(actMat, [], 2);
+
+
+peakOnlyData = nDataSet
+
+
+
+% timeStep  = DataSetList(nData).params.timeSeries;
+% timeTag   = timePoints(2):timePoints(4)+13; % sample to response
+% numTime   = length(timeTag);
+% polein    = DataSetList(nData).params.polein;
+% poleout   = DataSetList(nData).params.poleout;
+% 
+% countMaxId = hist(maxId, 1:numTimeBin*2)/size(actMat,1)*100;
+% std(countMaxId([timeTag, timeTag+numTimeBin]))%/mean(countMaxId([timeTag, timeTag+77]))
+% [bootstat,bootsam] = bootstrp(1000,@std,countMaxId([timeTag, timeTag+numTimeBin]));
+% std(bootstat)
+% figure;
+% hold on;
+% bplot = bar(timeStep, countMaxId(1:numTimeBin), 1, 'facecolor', 'b', 'edgecolor', 'none');
+% bplot.FaceAlpha = 0.5;
+% bplot = bar(timeStep, countMaxId(1+numTimeBin:end), 1, 'facecolor', 'r', 'edgecolor', 'none');
+% bplot.FaceAlpha = 0.5;
+% xlim([timeStep(1) params.timeSeries(end)])
+% ylim([0 8])
+% gridxy ([polein, poleout, 0],[1/(numTimeBin-8)/2*100], 'Color','k','Linestyle','--','linewid', 1.0)
+% box off
+% set(gca,'TickDir','out')
+% ylabel('% Max peak')
+% xlabel('Time')
+% hold off
