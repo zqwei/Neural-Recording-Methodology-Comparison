791f6d38472157d36362d32ad78390e68c221cba
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-01-28 18:10:30 -0500

    add code for long delay c2s

---
+++ C2SMLSpikeDenovModelDataAnalysis/generateModeledLongDelayDatasets.m
@@ -0,0 +1,61 @@
+% 
+% Comparison based on single unit acitivity
+% Generating Ca++ imaging data from ephys data using Tsai-Wen's model
+% 
+% -------------------------------------------------------------------------
+% version 1.0
+%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Common parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath('../Func');
+setDir;
+load ([TempDatDir 'DataListShuffle.mat']);
+tempDatOld = TempDatDir;
+minNumTrialToAnalysis  = 20;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% short Ca fast
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params.frameRate       =  29.68/2;
+params.binsize         =  1/params.frameRate;
+params.poleout         =  -3.0;
+params.polein          =  params.poleout -1.2;
+minTimeToAnalysis      =  round((params.polein - 0.5) * params.frameRate);
+maxTimeToAnalysis      =  round(2.0 * params.frameRate);
+params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
+params.timeSeries      = params.timeWindowIndexRange * params.binsize;
+params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
+params.expression      = 'Transgentic';
+load([tempDatOld DataSetList(5).name '_withOLRemoval.mat'])
+nDataSet               = getFakeMLSpikeCaImagingData(nDataSet, params.frameRate);  
+nonActiveNeuronIndex   = findNonActiveNeurons(nDataSet, params);
+nData                  = 1;
+DataSetList(nData).name    = 'ModelSpikeMLSpike_Deconv_Ca_Fast_Long_Delay';
+DataSetList(nData).params  = params; 
+DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
+save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% short Ca slow virus
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params.frameRate       =  29.68/2;
+params.binsize         =  1/params.frameRate;
+params.poleout         =  -3.0;
+params.polein          =  params.poleout -1.2;
+minTimeToAnalysis      =  round((params.polein - 0.5) * params.frameRate);
+maxTimeToAnalysis      =  round(2.0 * params.frameRate);
+params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
+params.timeSeries      = params.timeWindowIndexRange * params.binsize;
+params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
+params.expression      = 'Transgentic';
+load([tempDatOld DataSetList(6).name '_withOLRemoval.mat'])
+nDataSet               = getFakeMLSpikeCaImagingData(nDataSet, params.frameRate);
+nonActiveNeuronIndex   = findNonActiveNeurons(nDataSet, params);
+nData                  = 2;
+DataSetList(nData).name    = 'ModelSpikeMLSpike_Deconv_Ca_Slow_Long_Delay';
+DataSetList(nData).params  = params; 
+DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
+save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet');
--- C2SRandomDenovModelDataAnalysis/generateModeledDatasets.m
+++ C2SRandomDenovModelDataAnalysis/generateModeledDatasets.m
@@ -13,6 +13,7 @@
 addpath('../Func');
 setDir;
 load ([TempDatDir 'DataListShuffle.mat']);
+TempDatDirOld = '../TempDat_2019_01_28/';
 
 minNumTrialToAnalysis  = 20;
 
@@ -30,7 +31,7 @@ params.timeSeries      = params.timeWindowIndexRange * params.binsize;
 params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
 params.expression      = 'Transgentic';
 
-load([TempDatDir DataSetList(3).name '_withOLRemoval.mat'])
+load([TempDatDirOld DataSetList(3).name '_withOLRemoval.mat'])
 truncatedNormal        = truncate(makedist('Normal'), -1.5, 1.5);
 std_r                  = 0.0375; % 0; %0.0375;
 median_r               = 0.0927;
@@ -44,7 +45,7 @@ nData                  = 1;
 DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_Slow_Short_Delay';
 DataSetList(nData).params  = params;
 DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
-save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet');
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % short Ca slow virus
@@ -59,7 +60,7 @@ params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
 params.timeSeries      = params.timeWindowIndexRange * params.binsize;
 params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
 params.expression      = 'Virus';
-load([TempDatDir DataSetList(4).name '_withOLRemoval.mat'])
+load([TempDatDirOld DataSetList(4).name '_withOLRemoval.mat'])
 std_r                  = 0.0246; % 0; %0.0246;
 median_r               = 0.0505;
 std_d                  = 0.4588; % 0; %0.4588;
@@ -72,20 +73,8 @@ nData                  = 2;
 DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_Slow_Short_Delay_Virus';
 DataSetList(nData).params  = params;
 DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
-save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet');
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-save([TempDatDir 'DataListC2SRandomDeconvModel.mat'], 'DataSetList');
-
-
-
-
-
-addpath('../Func');
-setDir;
-load ([TempDatDir 'DataListC2SRandomDeconvModel.mat']);
-load([TempDatDir 'FineTuned6fNLParams.mat'], 'nlParams');
 minNumTrialToAnalysis  = 15;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % short Ca fast
@@ -102,9 +91,7 @@ params.timeSeries      = params.timeWindowIndexRange * params.binsize;
 params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
 params.expression      = 'Transgentic';
 
-nlParams(sum(isnan(nlParams), 2)>0, :) = [];
-
-load([TempDatDir 'Shuffle_Ca_Fast_SShort_Delay_withOLRemoval.mat'])
+load([TempDatDirOld 'Shuffle_Ca_Fast_SShort_Delay_withOLRemoval.mat'])
 truncatedNormal        = truncate(makedist('Normal'), -0.9, 1.5);
 std_r                  = 0.0222;
 median_r               = 0.0213;
@@ -118,5 +105,92 @@ nData                  = 3;
 DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_Fast_SShort_Delay';
 DataSetList(nData).params  = params;
 DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
-save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet');
-save([TempDatDir 'DataListC2SRandomDeconvModel.mat'], 'DataSetList');
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% short 6s-AAV S1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+params.frameRate       =  7;
+params.binsize         =  1/params.frameRate;
+params.polein          =  -2.07;
+params.poleout         =  -1.05;
+minTimeToAnalysis      =  round(-2.6 * params.frameRate);
+maxTimeToAnalysis      =  round(2.0 * params.frameRate);
+params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
+params.timeSeries      = params.timeWindowIndexRange * params.binsize;
+params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
+params.expression      = 'Virus';
+load([TempDatDirOld 'Shuffle_Ca_SP_S1_Slow_Short_Delay_Virus_withOLRemoval.mat'])
+std_r                  = 0.0246; % 0; %0.0246;
+median_r               = 0.0505;
+std_d                  = 0.4588; % 0; %0.4588;
+median_d               = 1.7064;
+tau_r                  = random(truncatedNormal, length(nDataSet), 1) *  std_r + median_r;
+tau_d                  = random(truncatedNormal, length(nDataSet), 1) *  std_d + median_d;
+nDataSet               = getFakeSpikeDeconvData(nDataSet, tau_r, tau_d, params);
+nonActiveNeuronIndex   = findNonActiveNeurons(nDataSet, params);
+nData                  = 4;
+DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_SP_S1_Slow_Short_Delay_Virus';
+DataSetList(nData).params  = params;
+DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% long Ca fast
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params.frameRate       =  29.68/2;
+params.binsize         =  1/params.frameRate;
+params.poleout         =  -3.0;
+params.polein          =  params.poleout -1.2;
+minTimeToAnalysis      =  round((params.polein - 0.5) * params.frameRate);
+maxTimeToAnalysis      =  round(2.0 * params.frameRate);
+params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
+params.timeSeries      = params.timeWindowIndexRange * params.binsize;
+params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
+params.expression      = 'Transgentic';
+load([TempDatDirOld 'Shuffle_Ca_Fast_Long_Delay_withOLRemoval.mat'])
+std_r                  = 0.0222;
+median_r               = 0.0213;
+std_d                  = 0.5390;
+median_d               = 0.5898;
+tau_r                  = random(truncatedNormal, length(nDataSet), 1) *  std_r + median_r;
+tau_d                  = random(truncatedNormal, length(nDataSet), 1) *  std_d + median_d;
+nDataSet               = getFakeSpikeDeconvData(nDataSet, tau_r, tau_d, params);
+nonActiveNeuronIndex   = findNonActiveNeurons(nDataSet, params);
+nData                  = 5;
+DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_Fast_Long_Delay';
+DataSetList(nData).params  = params;
+DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% long Ca slow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params.binsize         =  1/params.frameRate;
+params.poleout         =  -3.0;
+params.polein          =  params.poleout -1.2;
+minTimeToAnalysis      =  round((params.polein - 0.5) * params.frameRate);
+maxTimeToAnalysis      =  round(2.0 * params.frameRate);
+params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
+params.timeSeries      = params.timeWindowIndexRange * params.binsize;
+params.minNumTrialToAnalysis =  minNumTrialToAnalysis;
+load([TempDatDirOld 'Shuffle_Ca_Slow_Long_Delay_withOLRemoval.mat'])
+std_r                  = 0.0375; % 0; %0.0375;
+median_r               = 0.0927;
+std_d                  = 0.5374; % 0; %0.5374;
+median_d               = 1.2294;
+tau_r                  = random(truncatedNormal, length(nDataSet), 1) *  std_r + median_r;
+tau_d                  = random(truncatedNormal, length(nDataSet), 1) *  std_d + median_d;
+nDataSet               = getFakeSpikeDeconvData(nDataSet, tau_r, tau_d, params);
+nonActiveNeuronIndex   = findNonActiveNeurons(nDataSet, params);
+nData                  = 6;
+DataSetList(nData).name    = 'ModelSpikeRandom_Deconv_Ca_Slow_Long_Delay';
+DataSetList(nData).params  = params;
+DataSetList(nData).ActiveNeuronIndex = ~nonActiveNeuronIndex;
+save([TempDatDirOld DataSetList(nData).name '.mat'], 'nDataSet');
+
+
+save([TempDatDirOld 'DataListC2SRandomDeconvModel.mat'], 'DataSetList');
