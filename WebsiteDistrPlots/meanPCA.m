8f64e11e4ddf10aa0d3e6586b541305685a1eb44
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-07-08 14:29:00 -0400

    update code for distributions

--- WebsiteDistrPlots/meanPCA.m
+++ WebsiteDistrPlots/meanPCA.m
@@ -4,55 +4,56 @@
 
 addpath('../Func');
 setDir;
-load ([TempDatDir 'DataListShuffle.mat']);
-% load ([TempDatDir 'DataListC2SShuffle.mat']);
-% load ([TempDatDir 'DataListS2CShuffle.mat']);
-file_ext = {'Peakiness'};
-xi = 0.0:0.1:2.0; 
-color_ = [0.4313    0.4313    0.4313];
+xi = 0.0:0.01:1.0; 
+color_ = [0.4667    0.6745    0.1882];
 
 combinedParams = {{1}, {2}, {[1 2]}};
 margNames      = {'Stim', 'Time', 'Inter'};
 numTrials      = 100;
 numFold        = 100;
-
-numComps       = 10;
+numComps       = 3;
 ROCThres       = 0.50;
 
-for nData      = 1:length(DataSetList)
-    load([TempDatDir DataSetList(nData).name '.mat'])
-    oldDataSet          = nDataSet;
-    selectedNeuronalIndex = selectedHighROCneurons(oldDataSet, DataSetList(nData).params, ROCThres, selectedNeuronalIndex);
-    nDataSet              = oldDataSet(selectedNeuronalIndex);
-    decodability          = zeros(numFold, size(nDataSet(1).unit_yes_trial,2)); 
-    
-    evMat              = zeros(numFold, length(combinedParams), numComps);
-    for nFold          = 1:numFold
-        firingRates        = generateDPCAData(nDataSet, numTrials);
-        firingRatesAverage = nanmean(firingRates, ndims(firingRates));
-        pcaX               = firingRatesAverage(:,:);
-        firingRatesAverage = bsxfun(@minus, firingRatesAverage, mean(pcaX,2));
-        pcaX               = bsxfun(@minus, pcaX, mean(pcaX,2));
-        Xmargs             = dpca_marginalize(firingRatesAverage, 'combinedParams', combinedParams, 'ifFlat', 'yes');
-        totalVar           = sum(sum(pcaX.^2));
-        [~, S, Wpca] = svd(pcaX');
+fileList = {'DataListShuffle', 'DataListC2SShuffle', 'DataListS2CShuffle'};
+Result_ = '../../../Documents/DatasetComparison/public/results/nonDataDistr/';
 
-        PCAmargVar         = zeros(length(combinedParams), length(nDataSet));
-        for i=1:length(Xmargs)
-            PCAmargVar(i,:) = sum((Wpca' * Xmargs{i}).^2, 2)' / totalVar;
+for nlist = 1:3
+    load ([TempDatDir fileList{nlist} '.mat']);
+    for nData      = 1:length(DataSetList)
+        disp(DataSetList(nData).name);
+        load([TempDatDir DataSetList(nData).name '.mat'])
+        selectedNeuronalIndex = DataSetList(nData).ActiveNeuronIndex';
+        oldDataSet          = nDataSet;
+        selectedNeuronalIndex = selectedHighROCneurons(oldDataSet, DataSetList(nData).params, ROCThres, selectedNeuronalIndex);
+        nDataSet              = oldDataSet(selectedNeuronalIndex);
+        decodability          = zeros(numFold, size(nDataSet(1).unit_yes_trial,2)); 
+        evMat              = zeros(numFold, length(combinedParams), numComps);
+        for nFold          = 1:numFold
+            firingRates        = generateDPCAData(nDataSet, numTrials);
+            firingRatesAverage = nanmean(firingRates, ndims(firingRates));
+            pcaX               = firingRatesAverage(:,:);
+            firingRatesAverage = bsxfun(@minus, firingRatesAverage, mean(pcaX,2));
+            pcaX               = bsxfun(@minus, pcaX, mean(pcaX,2));
+            Xmargs             = dpca_marginalize(firingRatesAverage, 'combinedParams', combinedParams, 'ifFlat', 'yes');
+            totalVar           = sum(sum(pcaX.^2));
+            [~, S, Wpca] = svd(pcaX');
+
+            PCAmargVar         = zeros(length(combinedParams), length(nDataSet));
+            for i=1:length(Xmargs)
+                PCAmargVar(i,:) = sum((Wpca' * Xmargs{i}).^2, 2)' / totalVar;
+            end
+            evMat(nFold, :, :) = PCAmargVar(:, 1:numComps);
         end
-        evMat(nFold, :, :) = PCAmargVar(:, 1:numComps);
-    end
-    if nData == 1
-        refEphys.pca = evMat;
-        disp(mean(evMat(:, :, 1)))
-    end
-    if nData == 3
-        performanceMat(2).pca = evMat;
-        disp(mean(evMat(:, :, 1)))
-    end
-    if nData == 4
-        performanceMat(1).pca = evMat;
-        disp(mean(evMat(:, :, 1)))
+        for nPC = 1:3
+            figure;
+            tmp_s = evMat(:, 2, nPC)./sum(evMat(:, :, nPC), 2);
+            fs = ksdensity(tmp_s, xi);
+            f_ = max(fs);
+            plot(xi, fs/f_, '-', 'color', color_, 'linewid', 2);
+            set(gca,'XTick',[0, 1], 'YTick', [], 'TickDir', 'out','Ycolor','none')
+            box off
+            setPrint(8,3,[Result_ DataSetList(nData).name '_PC' num2str(nPC)], 'svg')
+            close all;
+        end
     end
 end
--- WebsiteDistrPlots/peakLocationDistrPosNeuronKL.m
+++ WebsiteDistrPlots/peakLocationDistrPosNeuronKL.m
@@ -1,75 +1,65 @@
 addpath('../Func');
 setDir;
-load ([TempDatDir 'DataListShuffle.mat']);
+file_ext = {'KL'};
+xi = 0:0.001:0.5; 
+color_ = [     0    0.4470    0.7410];
+fileList = {'DataListShuffle', 'DataListC2SShuffle', 'DataListS2CShuffle'};
+Result_ = '../../../Documents/DatasetComparison/public/results/nonDataDistr/';
 
-if ~exist([PlotDir 'SingleUnitsPeakLocation'],'dir')
-    mkdir([PlotDir 'SingleUnitsPeakLocation'])
-end
-
-for nData     = [1 3 4 10]%1:length(DataSetList)%[11 12]%
-    if ~exist([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'], 'file')
+for nlist = 1:3
+    load ([TempDatDir fileList{nlist} '.mat']);
+    for nData     = 1:length(DataSetList)
         load([TempDatDir DataSetList(nData).name '.mat'])
-        neuronRemoveList = false(length(nDataSet), 1);
-    else
-        load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
-    end
-    
-    disp(DataSetList(nData).name)
-    params      = DataSetList(nData).params;
-    timePoints  = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
-    
-    numTimeBin          = size(nDataSet(1).unit_yes_trial, 2);
-    yesProfileMatrix    = nan(length(nDataSet), numTimeBin);
-    noProfileMatrix     = yesProfileMatrix;
-    positivePeak        = false(length(nDataSet));
-    for nUnit        = 1:length(nDataSet)
-        yesData      = mean(nDataSet(nUnit).unit_yes_trial);
-        noData       = mean(nDataSet(nUnit).unit_no_trial);
-        maxData      = max([yesData, noData]);
-        minData      = min([yesData, noData]);
-        rData        = (maxData - minData);
-        yesData      = (yesData - minData)/(maxData - minData);
-        noData       = (noData - minData)/(maxData - minData);
-        yesProfileMatrix(nUnit, :)    = yesData;
-        noProfileMatrix(nUnit, :)     = noData;
-        positivePeak(nUnit)       = mean(yesData(timePoints(1):timePoints(2))) <= mean(yesData(timePoints(2):timePoints(4))) ...
-                                   || mean(noData(timePoints(1):timePoints(2))) <= mean(noData(timePoints(2):timePoints(4)));
+        disp(DataSetList(nData).name)
+        params      = DataSetList(nData).params;
+        timePoints  = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+        numTimeBin          = size(nDataSet(1).unit_yes_trial, 2);
+        yesProfileMatrix    = nan(length(nDataSet), numTimeBin);
+        noProfileMatrix     = yesProfileMatrix;
+        positivePeak        = false(length(nDataSet));
+        for nUnit        = 1:length(nDataSet)
+            yesData      = mean(nDataSet(nUnit).unit_yes_trial);
+            noData       = mean(nDataSet(nUnit).unit_no_trial);
+            maxData      = max([yesData, noData]);
+            minData      = min([yesData, noData]);
+            rData        = (maxData - minData);
+            yesData      = (yesData - minData)/(maxData - minData);
+            noData       = (noData - minData)/(maxData - minData);
+            yesProfileMatrix(nUnit, :)    = yesData;
+            noProfileMatrix(nUnit, :)     = noData;
+            positivePeak(nUnit)       = mean(yesData(timePoints(1):timePoints(2))) <= mean(yesData(timePoints(2):timePoints(4))) ...
+                                       || mean(noData(timePoints(1):timePoints(2))) <= mean(noData(timePoints(2):timePoints(4)));
 
-    end
-    actMat        = [yesProfileMatrix, noProfileMatrix];
-    actMat        = actMat(positivePeak, :);
-    [~, maxId]    = max(actMat, [], 2);
+        end
+        actMat        = [yesProfileMatrix, noProfileMatrix];
+        actMat        = actMat(positivePeak, :);
+        [~, maxId]    = max(actMat, [], 2);
 
-    timeStep  = DataSetList(nData).params.timeSeries;
-    timeTag   = timePoints(2):timePoints(4)+13; % sample to response
-    numTime   = length(timeTag);
-    polein    = DataSetList(nData).params.polein;
-    poleout   = DataSetList(nData).params.poleout;
-    kl        = zeros(nNeuron, 1);
-    for nNeuron    = 1:length(maxId)
-        maxId_     = maxId;
-        maxId_(nNeuron) = [];
-        countMaxId = (hist(maxId_, 1:numTimeBin*2)+1)/size(actMat,1);
-        countMaxId = countMaxId / sum(countMaxId);
-        % kl(nNeuron)= 1/length(countMaxId)*sum(log(countMaxId*length(countMaxId)));
-        [emd_, emd_val] = emd_distance((1:length(countMaxId))', (1:length(countMaxId))', (ones(size(countMaxId))/length(countMaxId))', countMaxId'/sum(countMaxId), @gdf);
-        kl(nNeuron)= emd_val;
+        timeStep  = DataSetList(nData).params.timeSeries;
+        timeTag   = timePoints(2):timePoints(4)+13; % sample to response
+        numTime   = length(timeTag);
+        polein    = DataSetList(nData).params.polein;
+        poleout   = DataSetList(nData).params.poleout;
+        kl        = zeros(length(maxId), 1);
+        for nNeuron    = 1:length(maxId)
+            maxId_     = maxId;
+            maxId_(nNeuron) = [];
+            countMaxId = (hist(maxId_, 1:numTimeBin*2)+1)/size(actMat,1);
+            countMaxId = countMaxId / sum(countMaxId);
+            kl(nNeuron)= 1/length(countMaxId)*sum(log(countMaxId*length(countMaxId)));
+        end
+        for n_ = 1:1
+            figure
+            tmp_s = -kl(:, n_);
+            fs = ksdensity(tmp_s, xi);
+            f_ = max(fs);
+            plot(xi, fs/f_, '-', 'color', color_, 'linewid', 2);
+            set(gca,'XTick',[xi(1), xi(end)], 'YTick', [], 'TickDir', 'out','Ycolor','none')
+            xlim([0, 0.5])
+            ylim([0, 1])
+            box off
+            setPrint(8,3,[Result_ DataSetList(nData).name '_' file_ext{n_}], 'svg')
+            close all;
+        end
     end
-    disp([mean(kl) std(kl)])
 end
-
-close all
-
-
-
-% % % % Shuffle_Spikes_Nuo_Short_Delay
-% % % %     5.5408    0.0183
-% % % % 
-% % % % Shuffle_Ca_Slow_Short_Delay
-% % % %     6.2397    0.0198
-% % % % 
-% % % % Shuffle_Ca_Slow_Short_Delay_Virus
-% % % %     3.0310    2.5735
-% % % % 
-% % % % Shuffle_Ca_Fast_SShort_Delay
-% % % %     5.1598    0.0101
