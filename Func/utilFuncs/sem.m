48ec3c9e4994b7cd72b4986cc9d595720fdeb054
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-08-09 22:52:35 -0400

    update sem

--- Func/utilFuncs/sem.m
+++ Func/utilFuncs/sem.m
@@ -1,15 +1,19 @@
 function y = sem(x)
     
-    if size(x, 2) > 1
-        y     = nanstd(x, [], 2);
-        % len_y = sum(~isnan(y));
-        len_y = length(x, 2);
-        y     = y / sqrt(len_y);
-    else
-        y = nanstd(x, [], 1);
-        % len_y = sum(~isnan(x));
-        len_y = size(x, 1);
-        y     = y / sqrt(len_y);
-    end
+%     if size(x, 2) > 1
+%         y     = nanstd(x, [], 2);
+%         % len_y = sum(~isnan(y));
+%         len_y = size(x, 2);
+%         y     = y / sqrt(len_y);
+%     else
+%         y = nanstd(x, [], 1);
+%         % len_y = sum(~isnan(x));
+%         len_y = size(x, 1);
+%         y     = y / sqrt(len_y);
+%     end
+
+    y = nanstd(x, [], 1);
+    len_y = size(x, 1);
+    y     = y / sqrt(len_y);
 
 end
